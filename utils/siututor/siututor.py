8332501dc565795502c7221d9fb40fd124ae4d21
Michael Chow <mc_al_github@fastmail.com>
2020-07-19 13:06:52 -0600

    add latest siututor

--- utils/siututor/siututor.py
+++ utils/siututor/siututor.py
@@ -1,8 +1,9 @@
 __version__ = "0.0.1"
 
 import siuba
-from siuba.dply.verbs import DPLY_FUNCTIONS
+from siuba.dply.verbs import DPLY_FUNCTIONS, NoArgs
 from functools import wraps
+import itertools
 
 class Blank:
     def __call__(self, *args, **kwargs):
@@ -24,22 +25,49 @@ class Blank:
 def blanks_wrapper(f):
     @wraps(f)
     def wrapper(*args, **kwargs):
-        all_vals = itertools.chain(args, kwargs.values)
+        all_vals = itertools.chain(args, kwargs.values())
         if any(isinstance(x, Blank) for x in all_vals):
             return Blank()
 
         return f(*args, **kwargs)
+    
+    return wrapper
+
+def dispatch_wrapper(f):
+    @wraps(f)
+    def wrapper(cls):
+        impl = f(cls)
+        return blanks_wrapper(impl)
+    
+    return wrapper
+
+def return_blank(*args, **kwargs):
+    return Blank()
 
 def wrap_func(func_name):
+    import pandas as pd
+    from pandas.core.groupby import DataFrameGroupBy
+
     verb = getattr(siuba, func_name)
     # TODO: fix joins not have register attribute
     if hasattr(verb, 'register'):
-        verb.register(Blank, lambda x, *args, **kwargs: Blank())
-        wrapped = blanks_wrapper(verb)
-        globals()[func_name] = wrapped
 
+        verb.register(Blank, return_blank)
+        verb.register(NoArgs, blanks_wrapper(verb.registry.get(NoArgs)))
+        verb.register(pd.DataFrame, blanks_wrapper(verb.registry.get(pd.DataFrame)))
+        verb.register(DataFrameGroupBy, blanks_wrapper(verb.registry.get(DataFrameGroupBy)))
 
+# Replaces verb dispatchers with one that always wraps the implementation
+# in blanks_wrapper. This allows it to propogate maybe under a number of cases
+# that would normally dispatch on regular verb, which may handle blanks in 
+# surprising ways. E.g.
+#    - data >> mutate(argname = ___)
+#    - mutate(data, argname = ___)
+#
+# Note that all other cases are handled by the Blanks class itself
+#    - data >> ___
+#    - data >> ___ >> ___
+#    - data >> ___()
+#    - data >> ___(___)
 for _func_name in DPLY_FUNCTIONS:
     wrap_func(_func_name)
-    
-
