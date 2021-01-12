e709c591fe2a9ded0f335d58af06df3f4dc9b088
Michael Chow <mc_al_github@fastmail.com>
2021-01-12 14:02:00 -0700

    fix: gridsome config to build course index page

--- gridsome.config.js
+++ gridsome.config.js
@@ -54,7 +54,7 @@ module.exports = {
       use: '@gridsome/vue-remark',
       options: {
         baseDir: './tutorial/intro-data-science',
-        path: '*/index.md',
+        path: 'index.md',
         typeName: 'CourseLanding',
         template: './src/templates/CourseLanding.vue',
         remark: {
